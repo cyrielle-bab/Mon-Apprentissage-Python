aa73ef8263cf5fa374746032cdba50140f629b73
Cyrielle-bab <cyriellebab@gmail.com>
2026-02-13 12:29:20 +0100

    Modification du code

--- Appolo 1.py
+++ Appolo 1.py
@@ -1,1 +1,13 @@
-print(" Good morning")
+import random
+
+prime = random.randint
+
+if prime == 80 :
+   print("Neymar")
+elif prime ==90 :
+   print("Messi")
+elif prime == 100 :
+   print("Ronaldo")
+else :
+   print("Mbappé")
+
